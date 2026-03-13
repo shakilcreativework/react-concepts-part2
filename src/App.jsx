8a901d5b4f003b461d735debcb11743224533175
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-03-13 15:44:40 +0600

    amazing, learning how can use onclick in a handle function

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,17 @@ function App() {
     console.log("I am clicked");
   };
 
+  function addition(num){
+    const plus = num + 5;
+    console.log(plus);
+  };
+
   return (
     <>
       <h1>Hello, world</h1>
       {/* btn */}
       <button onClick={handleClick}>Click me</button>
+      <button onClick={() => addition(7)}>Click 2</button>
     </>
   )
 }
